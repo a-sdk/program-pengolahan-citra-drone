6ea0ea4708b44ffaf24edafc8a8c13ef97ac3919
Nitro 5 <derpydepp23@gmail.com>
2025-09-20 13:58:45 +0700

    Ganti dikit

--- program_persiapan_gcp.py
+++ program_persiapan_gcp.py
@@ -1,8 +1,8 @@
 
 import pandas as pd
-
+import numpy as np
 # Menentukan file csv
-target_file = r"C:\Users\acer_\Documents\rumah.csv"
+target_file = r"C:\Users\acer_\Documents\tomo.csv"
 # Membaca file csv
 print("\nMemuat file...")
 df = pd.read_csv(target_file)
@@ -18,13 +18,15 @@ kolom[1], kolom[2] = kolom[2], kolom[1]
 # Memodifikasi urutan kolom dataframe
 df_urut = df[kolom]
 # Menentukan kolom yang tidak diperlukan (akan dihapus)
-hapus_kolom = ['Notes', 'DMS', 'UTM', 'MGRS', 'CRS', 'CRS Code', 'Address', 'Date Record', 'Photo1', 'Photo2', 'Photo3']
+hapus_kolom = ['Notes', 'DMS', 'UTM', 'MGRS', 'CRS', 'CRS Code', 'Address', 'Date Record', 'GPS Accuracy(m)', 'Photo1', 'Photo2', 'Photo3']
 # Menghapus kolom yang tidak perlu
 df_hasil = df_urut.drop(columns=hapus_kolom)
+# fill_values = {'Elevation(MSL)': 71.6}
+# df_hasil.fillna(value=fill_values, inplace=True)
 print("Menampilkan pratinjau hasil proses...")
 print(f"\n{df_hasil}")
 # Menyimpan hasil ke csv
 print("\nMenyimpan file...")
-df_hasil.to_csv('rumah2.csv', index=False)
+df_hasil.to_csv('tomo2.csv', index=False)
 print("File Berhasil Disimpan")
 
